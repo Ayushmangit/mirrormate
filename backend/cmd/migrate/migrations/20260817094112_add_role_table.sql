d686e4a1d44fce1e94b4cae8eee448008381bd25
Saurabhgit120 <Saurabhtch@gmail.com>
2026-08-17 15:34:39 +0530

    feat: added table for roles

---
+++ backend/cmd/migrate/migrations/20260817094112_add_role_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS roles(
+    id bigserial PRIMARY KEY,
+    name varchar(255) UNIQUE NOT NULL , 
+    description text ,
+    level INT NOT NULL DEFAULT 0
+);
+
+INSERT INTO ROLES(NAME,description,level )VALUES(
+    'user','A user can create post or update or comment',1)
+;
+INSERT INTO ROLES(NAME,description,level )VALUES(
+    'hr','Hr can  post review on users and all of the user privileges ',2);
+
+    INSERT INTO ROLES(NAME,description,level )VALUES(
+    'admin','An admin can create , edit or delete any post, comment and a,, privilege ',3);
+
+-- +goose Down 
+DROP TABLE IF EXISTS roles;
