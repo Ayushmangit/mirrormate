efead2b33a5e723258f0e53b1102b09817ec88d1
Saurabhgit120 <Saurabhtch@gmail.com>
2026-08-18 14:15:45 +0530

    Added auth and check for duplicate entry or email

--- backend/internal/store/storage.go
+++ backend/internal/store/storage.go
@@ -1,1 +1,28 @@
 package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrDuplicateEmail    = errors.New("New  payload")
+	ErrDuplicateUsername = errors.New("record not found")
+)
+
+type Storage struct {
+	Users interface {
+		Create(context.Context, *User) error
+	}
+
+	Roles interface {
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Users: &UserStorage{db},
+		Roles: &RoleStorage{db},
+	}
+}
